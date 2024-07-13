bc7fa3deebdd9a43e6ed7d06c16eb8293a7340da
Admin <1729374397@qq.com>
2024-07-13 10:33:40 +0800

    。。。。。

--- app/src/main/java/com/xwl/gitdemo/MainActivity.java
+++ app/src/main/java/com/xwl/gitdemo/MainActivity.java
@@ -3,9 +3,13 @@ package com.xwl.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity {
-
+    ReentrantLock
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
